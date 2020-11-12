acf20e003ca611d04927622d1516787afa851677
Jacob Wiltse <aquabug@gmail.com>
2020-11-12 18:59:40 -0500

    change the formatting of interactions a bit

--- interactions.go
+++ interactions.go
@@ -25,6 +25,7 @@ const (
 	InteractionApplicationCommand
 )
 
+// Response types
 const (
 	ResponsePong ResponseType = iota + 1
 	ResponseAcknowledge
@@ -32,68 +33,67 @@ const (
 	ResponseChannelMessageWithSource
 )
 
+// Response flags
 const (
 	ResponseFlagNormal    = 0
 	ResponseFlagEphemeral = 1 << 6
 )
 
-type (
-	ApplicationCommand struct {
-		ID            Snowflake                  `json:"id,omitempty"`
-		ApplicationID Snowflake                  `json:"application_id,omitempty"`
-		Name          string                     `json:"name"`
-		Description   string                     `json:"description"`
-		Options       []ApplicationCommandOption `json:"options"`
-		Handler       HandlerFunc                `json:"-"`
-	}
+type ApplicationCommand struct {
+	ID            Snowflake                  `json:"id,omitempty"`
+	ApplicationID Snowflake                  `json:"application_id,omitempty"`
+	Name          string                     `json:"name"`
+	Description   string                     `json:"description"`
+	Options       []ApplicationCommandOption `json:"options"`
+	Handler       HandlerFunc                `json:"-"`
+}
 
-	ApplicationCommandOption struct {
-		OptionType  ApplicationCommandOptionType     `json:"type"`
-		Name        string                           `json:"name"`
-		Description string                           `json:"description"`
-		Default     bool                             `json:"default"`
-		Required    bool                             `json:"required"`
-		Choices     []ApplicationCommandOptionChoice `json:"choices,omitempty"`
-		Options     []ApplicationCommandOption       `json:"options,omitempty"`
-	}
+type ApplicationCommandOption struct {
+	OptionType  ApplicationCommandOptionType     `json:"type"`
+	Name        string                           `json:"name"`
+	Description string                           `json:"description"`
+	Default     bool                             `json:"default"`
+	Required    bool                             `json:"required"`
+	Choices     []ApplicationCommandOptionChoice `json:"choices,omitempty"`
+	Options     []ApplicationCommandOption       `json:"options,omitempty"`
+}
 
-	ApplicationCommandOptionChoice struct {
-		Name  string      `json:"name"`
-		Value interface{} `json:"value"`
-	}
+type ApplicationCommandOptionChoice struct {
+	Name  string      `json:"name"`
+	Value interface{} `json:"value"`
+}
 
-	ApplicationCommandInteractionDataOption struct {
-		Name    string                                     `json:"name"`
-		Value   interface{}                                `json:"value,omitempty"`
-		Options []*ApplicationCommandInteractionDataOption `json:"options,omitempty"`
-	}
+type ApplicationCommandInteractionDataOption struct {
+	Name    string                                     `json:"name"`
+	Value   interface{}                                `json:"value,omitempty"`
+	Options []*ApplicationCommandInteractionDataOption `json:"options,omitempty"`
+}
 
-	ApplicationCommandInteractionData struct {
-		ID      Snowflake                                 `json:"id"`
-		Name    string                                    `json:"name"`
-		Options []ApplicationCommandInteractionDataOption `json:"options"`
-	}
+type ApplicationCommandInteractionData struct {
+	ID      Snowflake                                 `json:"id"`
+	Name    string                                    `json:"name"`
+	Options []ApplicationCommandInteractionDataOption `json:"options"`
+}
 
-	Interaction struct {
-		ID        Snowflake                          `json:"id"`
-		Type      InteractionType                    `json:"type"`
-		Data      *ApplicationCommandInteractionData `json:"data,omitempty"`
-		GuildID   Snowflake                          `json:"guild_id"`
-		ChannelID Snowflake                          `json:"channel_id"`
-		Member    GuildMember                        `json:"member"`
-		Token     string                             `json:"token"`
-	}
+type Interaction struct {
+	ID        Snowflake                          `json:"id"`
+	Type      InteractionType                    `json:"type"`
+	Data      *ApplicationCommandInteractionData `json:"data,omitempty"`
+	GuildID   Snowflake                          `json:"guild_id"`
+	ChannelID Snowflake                          `json:"channel_id"`
+	Member    GuildMember                        `json:"member"`
+	Token     string                             `json:"token"`
+}
 
-	InteractionApplicationCommandCallbackData struct {
-		TTS             bool             `json:"tts,omitempty"`
-		Content         string           `json:"content"`
-		Embeds          []*Embed         `json:"embeds,omitempty"`
-		AllowedMentions *AllowedMentions `json:"allowed_mentions,omitempty"`
-		Flags           int              `json:"flags"`
-	}
+type InteractionApplicationCommandCallbackData struct {
+	TTS             bool             `json:"tts,omitempty"`
+	Content         string           `json:"content"`
+	Embeds          []*Embed         `json:"embeds,omitempty"`
+	AllowedMentions *AllowedMentions `json:"allowed_mentions,omitempty"`
+	Flags           int              `json:"flags"`
+}
 
-	InteractionResponse struct {
-		Type ResponseType                               `json:"type"`
-		Data *InteractionApplicationCommandCallbackData `json:"data,omitempty"`
-	}
-)
+type InteractionResponse struct {
+	Type ResponseType                               `json:"type"`
+	Data *InteractionApplicationCommandCallbackData `json:"data,omitempty"`
+}
