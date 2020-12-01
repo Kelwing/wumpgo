e98a1b46606806cac1851c107c629dd4337d38e6
Jacob Wiltse <aquabug@gmail.com>
2020-12-01 12:39:24 -0500

    change header name

--- middleware.go
+++ middleware.go
@@ -9,10 +9,11 @@ import (
 
 func verifyMiddleware(h fasthttprouter.Handle, key ed25519.PublicKey) fasthttprouter.Handle {
 	return func(ctx *fasthttp.RequestCtx, ps fasthttprouter.Params) {
-		signature := ctx.Request.Header.Peek("x-signature-ed25519")
+		signature := ctx.Request.Header.Peek("X-Signature-Ed25519")
 		log.Println("Headers:", ctx.Request.Header.String())
-		log.Println("Body:", string(ctx.Request.Body()))
-		if verifyMessage(ctx.PostBody(), string(signature), key) {
+		body := ctx.PostBody()
+		log.Println("Body:", string(body))
+		if verifyMessage(body, string(signature), key) {
 			log.Println("signature verification succeeded")
 			h(ctx, ps)
 			return
