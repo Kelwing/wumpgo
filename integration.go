0d7e1e4dc82bf070ac8cc780f163f33d6611ec4e
Jack <40802798+Jack073@users.noreply.github.com>
2020-12-14 20:51:00 +0000

    add audit log and integration structs

---
+++ integration.go
@@ -0,0 +1,40 @@
+package objects
+
+type ExpireBehaviour uint
+
+const (
+	ExpireBehaviourRemoveRole ExpireBehaviour = iota
+	ExpireBehaviourKick
+)
+
+type Integration struct {
+	ID                Snowflake               `json:"id"`
+	Name              string                  `json:"name"`
+	Type              string                  `json:"type"`
+	Enabled           bool                    `json:"enabled"`
+	Syncing           bool                    `json:"syncing,omitempty"`
+	RoleID            Snowflake               `json:"role_id,omitempty"`
+	EnableEmoticons   bool                    `json:"enable_emoticons,omitempty"`
+	ExpireBehaviour   ExpireBehaviour         `json:"expire_behaviour,omitempty"`
+	ExpireGracePeriod int64                   `json:"expire_grace_period,omitempty"`
+	User              *User                   `json:"user,omitempty"`
+	Account           *IntegrationAccount     `json:"account,omitempty"`
+	SyncedAt          Time                    `json:"synced_at,omitempty"`
+	SubscriberCount   int64                   `json:"subscriber_count,omitempty"`
+	Revoked           bool                    `json:"revoked,omitempty"`
+	Application       *IntegrationApplication `json:"application,omitempty"`
+}
+
+type IntegrationAccount struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type IntegrationApplication struct {
+	ID          Snowflake `json:"id"`
+	Name        string    `json:"name"`
+	Icon        string    `json:"icon,omitempty"`
+	Description string    `json:"description"`
+	Summary     string    `json:"summary"`
+	Bot         *User     `json:"bot,omitempty"`
+}
