a58675f7d029b4b53986ca6b37833e776d8f26ad
Jake Gealer <jake@gealer.email>
2021-09-02 21:48:11 +0100

    fix a bug when member is nil

--- resolve_member.go
+++ resolve_member.go
@@ -5,10 +5,10 @@ import "github.com/Postcord/objects"
 // ResolveMember is used to attempt to resolve the item to a member. Returns nil if not a member.
 func (r ResolvableUser) ResolveMember() *objects.GuildMember {
 	snowflake := r.Snowflake()
-	x, ok := r.data.Resolved.Members[snowflake]
-	if ok {
+	if x, ok := r.data.Resolved.Members[snowflake]; ok {
 		u, _ := r.data.Resolved.Users[snowflake]
 		x.User = &u
+		return &x
 	}
-	return &x
+	return nil
 }
