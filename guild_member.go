ef45871ab3e71b07d98db0fc7530ed4f302cba49
Kelwing <aquabug@gmail.com>
2021-10-02 18:30:56 -0400

    add guild avatar and profile support

--- guild_member.go
+++ guild_member.go
@@ -3,6 +3,7 @@ package objects
 type GuildMember struct {
 	User         *User       `json:"user,omitempty"`
 	Nick         string      `json:"nick,omitempty"`
+	Avatar       string      `json:"avatar,omitempty"`
 	Roles        []Snowflake `json:"roles"`
 	JoinedAt     Time        `json:"joined_at"`
 	PremiumSince Time        `json:"premium_since,omitempty"`
--- user.go
+++ user.go
@@ -58,6 +58,8 @@ type User struct {
 	Bot           bool        `json:"bot,omitempty"`
 	System        bool        `json:"system,omitempty"`
 	MFAEnabled    bool        `json:"mfa_enabled,omitempty"`
+	Banner        string      `json:"banner,omitempty"`
+	AccentColor   int         `json:"accent_color"`
 	Locale        string      `json:"locale,omitempty"`
 	Verified      bool        `json:"verified,omitempty"`
 	Email         string      `json:"email,omitempty"`
