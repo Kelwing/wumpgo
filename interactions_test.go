b6aad647a877da099a8a517397e21afd9bbdacf8
Kelwing <aquabug@gmail.com>
2021-11-29 19:10:55 -0500

    improve tests

--- interactions_test.go
+++ interactions_test.go
@@ -135,6 +135,10 @@ func Test_HTTPHandler_FullEvent(t *testing.T) {
 		t.Errorf("Expected 200, got %d", w.Code)
 	}
 
+	if w.Header().Get("Content-Type") != "application/json" {
+		t.Errorf("Expected application/json, got %s", w.Header().Get("Content-Type"))
+	}
+
 	var resp objects.InteractionResponse
 	err = json.Unmarshal(w.Body.Bytes(), &resp)
 	if err != nil {
