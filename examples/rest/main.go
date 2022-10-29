46e5ff6bfe0e461720370fdcc5a643465f343c65
Kelwing <aquabug@gmail.com>
2022-10-29 00:40:26 -0400

    add rest example

---
+++ examples/rest/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+	"wumpgo.dev/wumpgo/rest"
+)
+
+func main() {
+	token := flag.String("token", "", "Discord token")
+	flag.Parse()
+
+	client := rest.New(&rest.Config{
+		Authorization: "Bot " + *token,
+		Ratelimiter:   rest.NewLeakyBucketRatelimiter(),
+		UserAgent:     "wumpgo/1.0",
+		Proxy:         http.ProxyFromEnvironment,
+	})
+
+	me, err := client.GetCurrentUser(context.Background())
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get current user")
+	}
+
+	log.Info().Str("username", me.Username).Str("discriminator", me.Discriminator).Msg("")
+}
